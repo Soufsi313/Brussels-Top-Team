28f73464fa37d24aa2ee15b88ea40051dc7da386
Soufiane MEFTAH <s.mef2703@gmail.com>
2026-08-29 10:49:58 +0200

    Connexion de la navigation entre les pages

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,55 @@
 <header>
 
     <div class="logo">
+
         <a href="index.php">
-            <img src="images/logo.jpg" alt="Logo Brussels Top Team">
+            <img src="images/logo.jpg" alt="Brussels Top Team">
         </a>
+
     </div>
 
+
     <nav>
+
         <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="#">Le BTT</a></li>
-            <li><a href="#">Disciplines</a></li>
-            <li><a href="#">Abonnements</a></li>
-            <li><a href="#">Contact</a></li>
+
+            <li>
+                <a href="index.php">
+                    Accueil
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    Disciplines
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    BTT Girls
+                </a>
+            </li>
+
+            <li>
+                <a href="abonnements.php">
+                    Abonnements
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    Contact
+                </a>
+            </li>
+
         </ul>
+
     </nav>
 
-    <a href="#" class="btn-inscription">Rejoindre le BTT</a>
+
+    <a href="abonnements.php" class="btn-inscription">
+        S'inscrire
+    </a>
 
 </header>
